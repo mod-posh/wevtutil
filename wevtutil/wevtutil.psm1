1f91ad7ff14b9debbd9ea747067bae2bb77ff558
Jeffrey Patton <jeffrey@patton-tech.com>
2020-09-16 16:12:47 -0500

    update get-loginfo

--- wevtutil/wevtutil.psm1
+++ wevtutil/wevtutil.psm1
@@ -22,45 +22,7 @@ Function Get-Log {
   }
 }
 Function Get-LogInfo {
-  <#
-  .SYNOPSIS
-  Displays status information about an event log or log file.
-  .DESCRIPTION
-  Displays status information about an event log or log file. If the
-  LogFile option is used, <Logname> is a path to a log file. You can
-  run Get-WevtLog -List to obtain a list of log names.
-  .PARAMETER Logname
-  The name of a log or path to a logfile/structured query file
-  .PARAMETER LogFile
-  Specifies that the events should be read from a log or from a log
-  file. <Logfile> can be true or false. If true, the parameter to the
-  command is the path to a log file.
-  .EXAMPLE
-  Get-WevtLogInfo -LogName Microsoft-Windows-CAPI2/Operational
-
-  creationTime: 2015-03-02T18:08:49.513Z
-  lastAccessTime: 2015-03-02T18:08:49.513Z
-  lastWriteTime: 2015-03-02T19:57:11.003Z
-  fileSize: 1052672
-  attributes: 32
-  numberOfLogRecords: 177
-  oldestRecordNumber: 1
-
-  Description
-  -----------
-  Get the log information for the CAPI log
-  .NOTES
-  FunctionName : Get-WevtLogInfo
-  Created by   : jspatton
-  Date Coded   : 03/02/2015 10:47:25
-  .LINK
-  https://github.com/jeffpatton1971/mod-posh/wiki/WevtUtil#Get-WevtLogInfo
-  .LINK
-  https://msdn.microsoft.com/en-us/library/windows/desktop/aa820708%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
-  .LINK
-  https://technet.microsoft.com/en-us/library/cc732848.aspx
-  #>
-  [CmdletBinding()]
+  [CmdletBinding(HelpURI = 'https://github.com/mod-posh/wevtutil/blob/master/docs/Get-LogInfo.md#get-loginfo')]
   Param
   (
     [Parameter(Mandatory = $true, ParameterSetName = 'get-loginfo')]
